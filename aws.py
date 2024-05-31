eaedcb5fff2e9110f835e79b931105dc79b92677
nyaparl <161057479+nyaparl@users.noreply.github.com>
2024-05-31 12:20:03 +0530

    Update aws.py

--- aws.py
+++ aws.py
@@ -1,23 +1,21 @@
 
 After createting the S3 bucket, we need add below bucket policy for coe deployment in S3 bucket
-
-                                                               {
-  "Id": "Policy1716964562825",
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "Stmt1716964561156",
-      "Action": [
-        "s3:GetObject"
-      ],
-      "Effect": "Allow",
-      "Resource": "arn:aws:s3:::mybucket-20240529-new/*",
-      "Principal": "*"
-    }
-  ]
+{
+    "Version": "2012-10-17",
+    "Id": "Policy1717138099633",
+    "Statement": [
+        {
+            "Sid": "Stmt1717138087935",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject",
+                "s3:PutObject"
+            ],
+            "Resource": "arn:aws:s3:::my-bucket-2024-05-31-new/*"
+        }
+    ]
 }
-
-
 Below Access are required for the Glue CICD pipeline creation
 
 codepipeline IAM role
